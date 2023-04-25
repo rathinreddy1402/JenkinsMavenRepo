fb1fdaeb8cc6e6329f4a6581c2e121c1723ae005
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-04-25 19:46:56 +0530

    jenkins push

--- JenkinsSample/src/test/java/Pratice/Sample.java
+++ JenkinsSample/src/test/java/Pratice/Sample.java
@@ -1,14 +1,33 @@
 package Pratice;
 
-public class Sample {
 
-	public static void main(String[] args) {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	@Test
+	public void m1()
+	{
+	WebDriver driver = null;
+	String BROWSER = System.getProperty("browser");
+	String URL = System.getProperty("url");
+	System.out.println(BROWSER);
+	if(BROWSER.equalsIgnoreCase("browser"))
+	{
+		WebDriverManager.chromedriver().setup();
+	   driver = new ChromeDriver();
+	}else if(BROWSER.equalsIgnoreCase("firefox"))
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+	}
 	
-		System.out.println("m1 running");
-		
-		//Push
-//pullBack
-		//jenks
+	driver.get(URL);
+	//push Jenkins
 	}
 
 }
